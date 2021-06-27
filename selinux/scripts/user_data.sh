1e0ff2ab67534321f598050776ad62cb028857ea
Maxim <max@injectiveprotocol.com>
2021-06-27 03:29:11 +0300

    user_data script fixed and works 🎉

--- selinux/scripts/user_data.sh
+++ selinux/scripts/user_data.sh
@@ -3,7 +3,7 @@
 # FOR RHEL 8 / CentOS 8
 
 # Some global variables
-CONFIG_MONIKER=selinux-test3
+CONFIG_MONIKER=selinux-test
 CONFIG_PERSISTENT_PEERS=9bca3e793fbc8aaf39b55888c0db875944d6ee11@3.14.183.40:26656,5f85fceaa8a1fe1fc0067ab9e5687d9c2105c03b@3.15.11.190:26656,fbcb0739d630d92d15edf3c864da028fcf468982@63.33.59.187:26656
 VALIDATOR_HOME=/home/injective.validator
 INJECTIVED_LOG_OUT=/var/log/injectived.log
@@ -33,6 +33,9 @@ sestatus
 echo "Setting SELinux policy to enforcing"
 setenforce 1
 
+echo "Enabling mmap for any domain"
+semanage boolean -m --on domain_can_mmap_files
+
 echo "Disallow user/staff to exec content from home and /tmp"
 semanage boolean -m --off user_exec_content
 semanage boolean -m --off staff_exec_content
@@ -51,22 +54,20 @@ groupadd staff
 usermod -a -G staff injective.dev
 
 wget https://raw.githubusercontent.com/InjectiveLabs/mainnet-config/7560af65908e963d952242ff233dc63c37ae8bc3/selinux/sudo/staff.sudo \
-	-O /etc/sudoers.d/staff.sudo
+	-O /etc/sudoers.d/staff
 
 echo "Copy authorized_keys to injective.dev"
 
 mkdir /home/injective.dev/.ssh
-chcon "staff_u:object_r:ssh_home_t:s0" /home/injective.dev/.ssh
+chmod 0700 /home/injective.dev/.ssh
 cp /root/.ssh/authorized_keys /home/injective.dev/.ssh/
-chcon "staff_u:object_r:ssh_home_t:s0" /home/injective.dev/.ssh/authorized_keys
 chown -R injective.dev:injective.dev /home/injective.dev/.ssh
 
 echo "Copy authorized_keys to injective.validator"
 
 mkdir /home/injective.validator/.ssh
-chcon "user_u:object_r:ssh_home_t:s0" /home/injective.validator/.ssh
+chmod 0700 /home/injective.dev/.ssh
 cp /root/.ssh/authorized_keys /home/injective.validator/.ssh/
-chcon "user_u:object_r:ssh_home_t:s0" /home/injective.validator/.ssh/authorized_keys
 chown -R injective.validator:injective.validator /home/injective.validator/.ssh
 
 echo "Setup update validator files tool"
@@ -77,12 +78,11 @@ wget https://github.com/InjectiveLabs/mainnet-config/releases/download/$UPDATE_V
 mv update-validator-files_linux_amd64/update-validator-files /usr/local/bin/
 rmdir update-validator-files_linux_amd64
 
-echo "Installing Geth binary for key management"
-wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.4-aa637fd3.tar.gz \
-	-O geth.tar.gz && tar xf geth.tar.gz && rm geth.tar.gz
-mv geth-linux-amd64-1.10.4-aa637fd3/geth /usr/local/bin/
-rm -rf geth-linux-amd64-1.10.4-aa637fd3
-chcon "system_u:object_r:bin_t:s0" /usr/local/bin/geth
+# echo "Installing Geth binary for key management"
+# wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.4-aa637fd3.tar.gz \
+# 	-O geth.tar.gz && tar xf geth.tar.gz && rm geth.tar.gz
+# mv geth-linux-amd64-1.10.4-aa637fd3/geth /usr/local/bin/
+# rm -rf geth-linux-amd64-1.10.4-aa637fd3
 
 echo "Download SELinux policies for injectived, peggo and updater"
 wget https://github.com/InjectiveLabs/mainnet-config/releases/download/$SELINUX_POLICIES_REV/injectived-validator.pp \
@@ -102,7 +102,8 @@ semodule -i /root/injective-update.pp
 # After policies are loaded and installed, we can set appropriate SELinux contexts to everything
 
 echo "Setting context for update-validator-files"
-chcon "system_u:object_r:injective_update_exec_t:s0" /usr/local/bin/update-validator-files
+semanage fcontext -a -t injective_update_exec_t /usr/local/bin/update-validator-files
+restorecon /usr/local/bin/update-validator-files
 
 echo "Creating injective.validator HOME layout with bin / config / data / peggo"
 
@@ -124,7 +125,13 @@ chcon "user_u:object_r:user_home_t:s0" $VALIDATOR_HOME/data
 chcon "user_u:object_r:user_home_t:s0" $VALIDATOR_HOME/peggo
 
 chcon "user_u:object_r:injectived_exec_t:s0" $VALIDATOR_HOME/bin/injectived
+semanage fcontext -a -t injectived_exec_t $VALIDATOR_HOME/bin/injectived
+restorecon $VALIDATOR_HOME/bin/injectived
+
 chcon "user_u:object_r:peggo_exec_t:s0" $VALIDATOR_HOME/bin/peggo
+semanage fcontext -a -t peggo_exec_t $VALIDATOR_HOME/bin/peggo
+restorecon $VALIDATOR_HOME/bin/peggo
+
 chcon "user_u:object_r:user_home_t:s0" $VALIDATOR_HOME/config/genesis.json
 chcon "user_u:object_r:user_home_t:s0" $VALIDATOR_HOME/peggo/.env
 
@@ -152,13 +159,15 @@ echo "Node config template replacing vars"
 sed -i 's|moniker = ""|moniker = "'$CONFIG_MONIKER'"|g' $VALIDATOR_HOME/config/config.toml
 sed -i 's|persistent_peers = ""|persistent_peers = "'${CONFIG_PERSISTENT_PEERS}'"|g' $VALIDATOR_HOME/config/config.toml
 
+chown -R injective.validator:injective.validator $VALIDATOR_HOME
+
 echo "Installing Systemd Unit files"
 
-wget $INJECTIVED_SERVICE_UNIT -O /etc/systemd/system/injective.service
+wget $INJECTIVED_SERVICE_UNIT -O /etc/systemd/system/injectived.service
 wget $PEGGO_SERVICE_UNIT -O /etc/systemd/system/peggo.service
 
 systemctl daemon-reload
-systemctl enable injective
+systemctl enable injectived
 
 echo "[NOTE] Injectived enabled, not started"
 echo "[NOTE] Peggo not enabled, not started"
@@ -167,13 +176,13 @@ echo "Prepare Systemd Unit logging output"
 
 touch $INJECTIVED_LOG_OUT
 chown -R injective.validator:injective.validator $INJECTIVED_LOG_OUT
-chcon system_u:object_r:injectived_log_t:s0 $INJECTIVED_LOG_OUT
-ls -Z $INJECTIVED_LOG_OUT
+semanage fcontext -a -t injectived_log_t $INJECTIVED_LOG_OUT
+restorecon $INJECTIVED_LOG_OUT
 
 touch $PEGGO_LOG_OUT
 chown -R injective.validator:injective.validator $PEGGO_LOG_OUT
-chcon system_u:object_r:peggo_log_t:s0 $PEGGO_LOG_OUT
-ls -Z $PEGGO_LOG_OUT
+semanage fcontext -a -t peggo_log_t $PEGGO_LOG_OUT
+restorecon $PEGGO_LOG_OUT
 
 echo "Moving SSH port to 2200"
 semanage port -a -t ssh_port_t -p tcp 2200
@@ -205,9 +214,10 @@ semanage fcontext -a -t validator_config_t -- "/validator(/.*)?"
 restorecon -Rv /validator
 chown -R injective.validator:injective.validator /validator
 
-echo "Remap unconfined root to sysadm"
-semanage login -m -S targeted -s "sysadm_u" -r s0-s0:c0.c1023 root
+echo "[ON THE NEXT BOOT] Remap unconfined root to sysadm"
+echo semanage login -m -S targeted -s sysadm_u -r s0-s0:c0.c1023 root >> /etc/rc.d/rc.local
 
-echo "Prevent any future SELinux policy changes"
-semanage boolean -m --on secure_mode_policyload
+echo "[ON THE NEXT BOOT] Prevent any future SELinux policy changes"
+echo semanage boolean -m --on secure_mode_policyload >> /etc/rc.d/rc.local
 
+sleep 20 && reboot &
