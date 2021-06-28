e01d02d7c063a611419f6afd0db0b3d77fffad6c
nhannamsiu <dangduynam95@gmail.com>
2021-06-28 15:58:11 +0700

    Remove UPDATE_VALIDATOR_FILES_REV var; add ssh, network and disk config

--- selinux/scripts/user_data.sh
+++ selinux/scripts/user_data.sh
@@ -19,12 +19,28 @@ PEGGO_SERVICE_UNIT=https://raw.githubusercontent.com/InjectiveLabs/mainnet-confi
 CHAIN_BIN_RELEASE_TAG=v4.0.9-1624286601
 SELINUX_POLICIES_REV=selinux-policies-1
 INIT_VALIDATOR_FILES_REV=tool-release-3
-UPDATE_VALIDATOR_FILES_REV=tool-release-2
 
 set -e
 
+echo "Add sentry ssh pub key to validator"
+mkdir pubkeys && cd pubkeys
+wget https://gist.githubusercontent.com/albertchon/f0e4fa73410a56c69499b2a888c96d43/raw/46ac2be50a07110aec110a14a5f982fef1a5e4e3/asia-sentry-0.pub
+wget https://gist.githubusercontent.com/albertchon/87a12161f5c3b6186cb8bf2f7d606675/raw/bc4c0511440d57ec4e1622e0e93df97358ffdf9d/eu-sentry-0.pub
+wget https://gist.githubusercontent.com/albertchon/c30fc5cc72b23a0ad8db4131ae9fb1d1/raw/1f466ee6852e59ca18ed193697e439a0039cc974/eu-sentry-1.pub
+wget https://gist.githubusercontent.com/albertchon/f8b17b25a24f1861978c14e901d65de2/raw/a8547bcef6ad44b4e2bb01ce7fd4570b3315b016/us-sentry-0.pub
+cat asia-sentry-0.pub >> /root/.ssh/authorized_keys
+cat eu-sentry-0.pub >> /root/.ssh/authorized_keys
+cat eu-sentry-1.pub >> /root/.ssh/authorized_keys
+cat us-sentry-0.pub >> /root/.ssh/authorized_keys
+cd /root/
+
+echo "Config VLAN for private network"
+sed -i 's|IPADDR=.*|IPADDR=192.168.10.2|g' /etc/sysconfig/network-scripts/ifcfg-bond0
+sed -i 's|NETMASK=.*|NETMASK=255.255.255.0|g' /etc/sysconfig/network-scripts/ifcfg-bond0
+sed -i 's|GATEWAY=.*|GATEWAY=192.168.10.1|g' /etc/sysconfig/network-scripts/ifcfg-bond0
+
 yum update -y
-yum install -y unzip wget tree nano \
+yum install -y unzip wget tree nano e4fsprogs \
 	selinux-policy selinux-policy-targeted setools-console \
 	policycoreutils-python-utils
 
@@ -43,8 +59,22 @@ semanage boolean -m --off staff_exec_content
 echo "Check SELinux policy:"
 getenforce
 
+echo "Mount disk to /home/injective.validator"
+mkdir $VALIDATOR_HOME
+parted --script /dev/nvme0n1 \
+	mklabel gpt \
+	mkpart injectived ext4 1MB 3841GB \
+	print \
+	quit
+lsblk
+sleep 5
+mkfs.ext4 /dev/nvme0n1p1
+echo "/dev/nvme0n1p1 $VALIDATOR_HOME xfs defaults 0 0" >> /etc/fstab
+mount /dev/nvme0n1p1 $VALIDATOR_HOME
+
 echo "Adding injective.validator user of type user_u"
-useradd -Z user_u injective.validator
+useradd -Z user_u -d $VALIDATOR_HOME -M injective.validator
+chown -R injective.validator:injective.validator $VALIDATOR_HOME
 
 echo "Adding injective.dev user of type staff_u"
 useradd -Z staff_u injective.dev
@@ -57,26 +87,22 @@ wget https://raw.githubusercontent.com/InjectiveLabs/mainnet-config/7560af65908e
 	-O /etc/sudoers.d/staff
 
 echo "Copy authorized_keys to injective.dev"
-
 mkdir /home/injective.dev/.ssh
 chmod 0700 /home/injective.dev/.ssh
 cp /root/.ssh/authorized_keys /home/injective.dev/.ssh/
 chown -R injective.dev:injective.dev /home/injective.dev/.ssh
 
 echo "Copy authorized_keys to injective.validator"
-
 mkdir /home/injective.validator/.ssh
 chmod 0700 /home/injective.dev/.ssh
 cp /root/.ssh/authorized_keys /home/injective.validator/.ssh/
 chown -R injective.validator:injective.validator /home/injective.validator/.ssh
 
 echo "Setup update validator files tool"
-
-wget https://github.com/InjectiveLabs/mainnet-config/releases/download/$UPDATE_VALIDATOR_FILES_REV/update-validator-files_linux_amd64.zip \
+wget https://github.com/InjectiveLabs/mainnet-config/releases/download/$INIT_VALIDATOR_FILES_REV/init-validator-files_linux_amd64.zip \
 	-O tmp.zip && unzip tmp.zip && rm tmp.zip
-
-mv update-validator-files_linux_amd64/update-validator-files /usr/local/bin/
-rmdir update-validator-files_linux_amd64
+mv init-validator-files_linux_amd64/init-validator-files /usr/local/bin/
+rmdir init-validator-files_linux_amd64
 
 # echo "Installing Geth binary for key management"
 # wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.4-aa637fd3.tar.gz \
@@ -100,13 +126,11 @@ echo "Installing injective-update.pp"
 semodule -i /root/injective-update.pp
 
 # After policies are loaded and installed, we can set appropriate SELinux contexts to everything
-
 echo "Setting context for update-validator-files"
 semanage fcontext -a -t injective_update_exec_t /usr/local/bin/update-validator-files
 restorecon /usr/local/bin/update-validator-files
 
 echo "Creating injective.validator HOME layout with bin / config / data / peggo"
-
 mkdir $VALIDATOR_HOME/bin
 mkdir $VALIDATOR_HOME/config
 mkdir $VALIDATOR_HOME/data
